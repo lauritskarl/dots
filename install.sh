7bd7c2407d6e872c1f0cf5ebbf7c45d70088a457
Karl Hans Laurits <karl@laurits.dev>
2025-08-03 23:35:02 +0300

    Initial commit

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+mkdir -p "$HOME/.config/fish"
+mkdir -p "$HOME/.config/git"
+mkdir -p "$HOME/.config/mise"
+mkdir -p "$HOME/.config/atuin"
+mkdir -p "$HOME/.config/helix"
+
+ln -sf "$PWD/.config/fish/config.fish" "$HOME/.config/fish/config.fish"
+ln -sf "$PWD/.config/git/config" "$HOME/.config/git/config"
+ln -sf "$PWD/.config/git/ignore" "$HOME/.config/git/ignore"
+ln -sf "$PWD/.config/mise/config.toml" "$HOME/.config/mise/config.toml"
+ln -sf "$PWD/.config/atuin/config.toml" "$HOME/.config/atuin/config.toml"
+ln -sf "$PWD/.config/helix/config.toml" "$HOME/.config/helix/config.toml"
+ln -sf "$PWD/.config/helix/languages.toml" "$HOME/.config/helix/languages.toml"
+
+gh auth setup-git
+gh extension install github/gh-copilot
+gh extension install dlvhdr/gh-dash
