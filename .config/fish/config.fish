09d700fd4669ace882ba83ec38bad72477d59e9f
Karl Hans Laurits <karl@laurits.dev>
2025-08-15 13:15:04 +0300

    Update dotfiles

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,5 @@
 if status is-interactive
+    fish_add_path /opt/homebrew/bin
     set --global fish_greeting
     set --global fish_key_bindings fish_vi_key_bindings
     set --global --export SHELL fish
