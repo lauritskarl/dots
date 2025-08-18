3fba7de5fe62f37e92da364e27fe4bc17a8a0fba
Karl Hans Laurits <karl@laurits.dev>
2025-08-18 16:35:43 +0300

    Set env variables using atuin instead

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,10 +2,6 @@ if status is-interactive
     fish_add_path /opt/homebrew/bin
     set --global fish_greeting
     set --global fish_key_bindings fish_vi_key_bindings
-    set --global --export SHELL fish
-    set --global --export EDITOR hx
-    set --global --export PAGER bat
-    set --global --export _ZO_ECHO 1
     mise activate fish | source
     atuin init fish | source
     zoxide init fish --cmd cd | source
