98a2b422fa7901b7714aafd44fd27bedba7554fc
Karl Hans Laurits <karl@laurits.dev>
2025-12-30 13:55:55 +0200

    Remove .config/fish/functions/ls.fish

--- private_dot_config/private_fish/functions/ls.fish
+++
@@ -1,3 +0,0 @@
-function ls --wraps='eza --long --icons --hyperlink --all --group-directories-first --smart-group --git --git-repos' --description 'alias ls eza --long --icons --hyperlink --all --group-directories-first --smart-group --git --git-repos'
-    eza --long --icons --hyperlink --all --group-directories-first --smart-group --git --git-repos $argv
-end
