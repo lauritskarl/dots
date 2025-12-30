7d4187b0a2fb71c2ef286cacbf1c0ddfcf72e8c6
Karl Hans Laurits <karl@laurits.dev>
2025-12-30 13:54:46 +0200

    Add .config/fish/functions/ls.fish

---
+++ private_dot_config/private_fish/functions/ls.fish
@@ -0,0 +1,3 @@
+function ls --wraps='eza --long --icons --hyperlink --all --group-directories-first --smart-group --git --git-repos' --description 'alias ls eza --long --icons --hyperlink --all --group-directories-first --smart-group --git --git-repos'
+    eza --long --icons --hyperlink --all --group-directories-first --smart-group --git --git-repos $argv
+end
