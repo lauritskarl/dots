f03ba28ee981c47450ae253ab671898ffe4f2b5e
Karl Hans Laurits <karl@laurits.dev>
2026-01-09 21:57:53 +0200

    Update .config/ghostty/config Update .config/fish/config.fish Remove .config/fish/functions/cm.fish Remove .config/fish/functions/ld.fish Remove .config/fish/functions/lg.fish Remove .config/fish/functions/lj.fish Remove .config/fish/functions/ls.fish Remove .config/fish/functions/lt.fish Remove .config/fish/functions/oc.fish Remove .config/fish/functions/y.fish

--- private_dot_config/private_fish/functions/lj.fish
+++
@@ -1,3 +0,0 @@
-function lj --wraps=lazyjournal --description 'alias lj lazyjournal'
-    lazyjournal $argv
-end
--- private_dot_config/private_fish/functions/lt.fish
+++
@@ -1,8 +0,0 @@
-function lt --description 'Tree listing with eza'
-    set -l level 2
-    if test (count $argv) -gt 0; and string match -qr '^\d+$' -- $argv[1]
-        set level $argv[1]
-        set -e argv[1]
-    end
-    eza --long --tree --icons --hyperlink --level=$level --all --group-directories-first --git-ignore --ignore-glob=.git --smart-group --git --git-repos $argv
-end
